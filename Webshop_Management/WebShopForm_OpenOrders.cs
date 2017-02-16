f9a7ac5168df261852e3bb0ac7dda67b0b1ea855
Nicolas Röchter <rr@tobit.com>
2017-02-16 10:14:45 +0100

    Add methods for filling the ItemList

--- Webshop_Management/WebShopForm_OpenOrders.cs
+++ Webshop_Management/WebShopForm_OpenOrders.cs
@@ -10,8 +10,30 @@ using System.Windows.Forms;
 
 namespace Webshop_Management {
     public partial class WebShopForm_OpenOrders : Form {
+
+        List<Order> openOrderList = new List<Order>();
+
         public WebShopForm_OpenOrders () {
             InitializeComponent();
+            cbbxDatabase.SelectedIndex = 2;
+
+            if (lstbxOrders.Items.Count > 0)
+                lstbxOrders.SelectedIndex = 0;
+
+            LoadItemsFromSQL();
+            FillItemList();
+        }
+
+        private void FillItemList () {
+            lstbxOrders.Items.Clear();
+            openOrderList.Sort((x, y) => x.BillingNumber.CompareTo(y.BillingNumber));
+
+            foreach (var currentOrder in openOrderList)
+                lstbxOrders.Items.Add(currentOrder.BillingNumber);
+        }
+
+        private void LoadItemsFromSQL () {
+            //LOAD COMPLETED ORDERS FROM SQL
         }
     }
 }

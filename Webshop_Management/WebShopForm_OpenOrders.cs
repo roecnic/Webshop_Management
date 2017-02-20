9033d5257a838962f42fa97def6ea5f092062da8
Nicolas Röchter <rr@tobit.com>
2017-02-20 16:24:36 +0100

    Remove redundand qualifiers; add lists

--- Webshop_Management/WebShopForm_OpenOrders.cs
+++ Webshop_Management/WebShopForm_OpenOrders.cs
@@ -1,19 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Webshop_Management {
     public partial class WebShopForm_OpenOrders : Form {
 
-        List<Order> openOrderList = new List<Order>();
-        List<Product> productList = new List<Product>();
-        List<Customer> customerList = new List<Customer>();
+        private List<Order> openOrderList = new List<Order>();
+        private List<Product> productList = new List<Product>();
+        private List<Product> shoppingCartProductList = new List<Product>();
+        private List<Customer> customerList = new List<Customer>();
+
 
         public WebShopForm_OpenOrders () {
             InitializeComponent();

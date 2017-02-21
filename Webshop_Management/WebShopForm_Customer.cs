e1b67c0eeb71564e7f45efef3e6bb9e01d2eb6b2
Nicolas Röchter <rr@tobit.com>
2017-02-21 11:10:05 +0100

    Add buildup for Form_Customer, add fill up methods

--- Webshop_Management/WebShopForm_Customer.cs
+++ Webshop_Management/WebShopForm_Customer.cs
@@ -10,8 +10,43 @@ using System.Windows.Forms;
 
 namespace Webshop_Management {
     public partial class WebShopForm_Customer : Form {
-        public WebShopForm_Customer() {
+
+        private List<Customer> customerList = new List<Customer>();
+        private String sortAlgorithm = "Lastname";
+
+        public WebShopForm_Customer () {
             InitializeComponent();
+            cbbxDatabase.SelectedIndex = 1;
+
+            LoadItemsFromSQL();
+            FillItemList();
+
+            if (lstbxCustomers.Items.Count > 0)
+                lstbxCustomers.SelectedIndex = 0;
+        }
+
+        /**
+         * ---------------------------------------------------------------
+         * STARTUP (FILL LISTS ETC.)
+         * ---------------------------------------------------------------
+         */
+
+        private void LoadItemsFromSQL () {
+            //LOAD CUSTOMERS FROM SQL
+            customerList.Add(new Customer(0, "Peter", "Sommer", 12345, "Vreden", 'M'));
+            customerList.Add(new Customer(1, "Nicolas", "Röchter", 48653, "Coesfeld", 'M'));
+            customerList.Add(new Customer(2, "Tobias", "Groten", 48683, "Ahaus", 'M'));
+        }
+
+        private void FillItemList () {
+            lstbxCustomers.Items.Clear();
+            if (sortAlgorithm.Equals("Lastname"))
+                customerList.Sort((x, y) => x.Lastname.CompareTo(y.Lastname));
+            else if (sortAlgorithm.Equals("ID"))
+                customerList.Sort((x, y) => x.ID.CompareTo(y.ID));
+
+            foreach (var currentCustomer in customerList)
+                lstbxCustomers.Items.Add(currentCustomer.ID + " - " + currentCustomer.Lastname + ", " + currentCustomer.Firstname);
         }
 
     }
